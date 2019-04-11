525bc4ff8c9d529d617a1efd0772d235e26a2fc5
Julie Butisbauch <harukaju@gmail.com>
2019-04-11 13:59:24 -0400

    images, initial js

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function displayContact() {
+    $('#js-content').empty();
+    document.getElementById("js-content").innerHTML = 
+    ``;
+}
+
+function displayPortfolio() {
+    $('#js-content').empty();
+    document.getElementById("js-content").innerHTML = 
+    ``;
+}
+
+function displayProfile(){
+    $('#js-content').empty();
+    document.getElementById("js-content").innerHTML = 
+    ``;
+}
+
+function displayResume() {
+    $('#js-content').empty();
+    document.getElementById("js-content").innerHTML = 
+    ``;
+}
+
